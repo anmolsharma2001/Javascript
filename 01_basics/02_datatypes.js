a09496d6376bde83ffd4a494d16592f158a14cba
anmolsharma2001 <144166824+anmolsharma2001@users.noreply.github.com>
2024-07-03 16:03:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"Use Strict"; // treat all js as newer version
+
+//alert(3 + 3)// we are using node js, not browser
+
+let name = "Anmol"
+let age = 18
+let isloggedIn = false
+let state;
+
+//Data Types//
+
+//1. Number => 2 to power 53
+// bigint 
+// String => ""
+//boolean => true/false
+//null => standalone value
+// undefine => 
+// symbol => uniqness ke liye
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
