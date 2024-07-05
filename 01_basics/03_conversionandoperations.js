dfe80c9f4f8f738b4a42b3de44706c006e24d6f7
anmolsharma2001 <144166824+anmolsharma2001@users.noreply.github.com>
2024-07-05 14:02:06 +0000

    operations

--- 01_basics/03_conversionandoperations.js
+++ 01_basics/03_conversionandoperations.js
@@ -20,9 +20,49 @@
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+// ************************************** operations *************************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // power
+// console.log(2/3);
+// console.log(2%2); 
+
+let str1 = "hello"
+let str2 = " Anmol"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + "2");
+// console.log("1" + 2 + 2 );
+
+// console.log(1 + 2 + "2" );
+
+
+// console.log(3 + 4 * 5 % 3); // wrong way use parentheses 
+
+// console.log(true+);
+// console.log(+"");
+
+let num1, num2, num3 // bad practice
+
+num1 = num1 = num3 = 2 + 2 // bad practice
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
