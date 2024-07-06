4cd7b46cc7ee6c6bf87d1864c603eecc72260085
anmolsharma2001 <144166824+anmolsharma2001@users.noreply.github.com>
2024-07-06 11:56:55 +0000

    stack and heap memory

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -15,15 +15,39 @@ const bigNumber = 3456543576654356754n
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"]
+// const heros = ["shaktiman", "naagraj", "doga"]
 
-let myObj = {
-  name : "Anmol",
-  age : 20
-}
+// let myObj = {
+//   name : "Anmol",
+//   age : 20
+// }
 
-let myFunction = function() {
-    console.log("Hello world");
+// let myFunction = function() {
+//     console.log("Hello world");
+// };
+
+// console.log(typeof myFunction);
+
+// ***********************************************************************************************
+
+// Stack(primitive ), Heap (Non-primitive)
+
+let myYoutubename = "Anmolsharmadotcom"
+
+let anothername = myYoutubename
+anothername = "chaiorcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@ybl"
 };
 
-console.log(typeof myFunction);
+let userTwo = userOne
+
+userTwo.email = "anmol@goggle.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
