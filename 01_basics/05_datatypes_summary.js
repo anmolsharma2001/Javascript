e2884028dd668c575286fa7a335ec2924f865e53
anmolsharma2001 <144166824+anmolsharma2001@users.noreply.github.com>
2024-07-06 03:38:21 +0000

    datatype_summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,29 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')  // symbol
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+  name : "Anmol",
+  age : 20
+}
+
+let myFunction = function() {
+    console.log("Hello world");
+};
+
+console.log(typeof myFunction);
